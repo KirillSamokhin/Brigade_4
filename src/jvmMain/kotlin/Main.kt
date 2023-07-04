cf3aac543ff550a86c81f49ee1002d9d07fb4b99
1that <GeorgiOlio@yandex.ru>
2023-07-04 18:50:59 +0300

    Interface_Alpha2

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,10 +1,13 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpSize
@@ -14,50 +17,122 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
+/* Это база :)*/
+enum class Base {
+    Grass,
+    Water,
+    Stone
+}
 
-//fun app() {
-//    var text by remember { mutableStateOf("Hello, World!") }
-//
-//    MaterialTheme {
-//        Button(onClick = {
-//            text = "Hello, Desktop!"
-//        }) {
-//            Text(text)
-//        }
-//    }
-//}
+/* Это я не знаю что */
+enum class Function {
+    Simple,
+    Start,
+    Finish
+}
 
-private class Controller {
-    var flagAnswer = false
+/* Состояния */
+enum class State {
+    Simple,
+    InOpenList,
+    InClosedList
+}
+
+class CellView {
     @Composable
-    fun project () {
-        var inputValueX by remember { mutableStateOf("") }
-        var inputValueY by remember { mutableStateOf("") }
-        val isVisibleX by remember {
-            derivedStateOf {
-                inputValueX.isNotBlank()
-            }
-        }
-        val isVisibleY by remember {
-            derivedStateOf {
-                inputValueY.isNotBlank()
-            }
-        }
+    fun make_box (cell: Cell) {
         Box (
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .padding(1.dp)
+                .aspectRatio(1f)
+                .background(Color(get_color(cell.type))),
+            contentAlignment = Center
         ) {
-            Box (
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.TopEnd
+            Text (text=cell.get_info().toString(), fontSize = 30.sp)
+        }
+    }
+    fun get_color (type: Triple<Base, Function, State>): Long {
+        var res: Long = 0x00000000
+        res = when(type.first) {
+            Base.Grass -> 0xff649500
+            Base.Water -> 0xff87cefa
+            Base.Stone -> 0xff696969
+        }
+
+        res += when(type.third) {
+            State.InOpenList -> 0xccd3d3d3
+            State.InClosedList -> 0xcca9a9a9
+            else -> 0
+        }
+        return res
+    }
+}
+
+
+
+class Cell (
+    var type: Triple<Base, Function, State> = Triple(Base.Grass, Function.Simple, State.Simple),
+    var h: Int = 0,
+    var g: Int = 0,
+    var f: Int = 0
+) {
+    fun get_info(): Triple<Int, Int, Int> {
+        return Triple(h, g, f)
+    }
+    fun set_f(a: Int) {
+        f = a
+    }
+}
+
+class FieldView {
+    /* Метод рисования поля */
+    @Composable
+    fun drawField (x: Int, y: Int) {
+        Row {
+            /* Пространство слева */
+            Box(
+                modifier = Modifier
+                    .weight(1f) // цена
+                    .padding(16.dp)
+                    .fillMaxHeight()
             ) {
-                Column(
-                    modifier = Modifier.padding(
-                        start = 20.dp,
-                        top = 20.dp,
-                        end = 20.dp,
-                        bottom = 20.dp
-                    ),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                val field = Array(20) { Cell() }
+                field[3].type = Triple(Base.Water, Function.Simple, State.Simple)
+                field[6].type = Triple(Base.Water, Function.Simple, State.Simple)
+                field[8].type = Triple(Base.Water, Function.Simple, State.Simple)
+                field[0].type = Triple(Base.Stone, Function.Simple, State.Simple)
+                val cellview = CellView()
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(5),
+                    content = {
+                        items(20) { i ->
+                            cellview.make_box(field[i])
+                        }
+                    }
+                )
+            }
+            /* Пространство справа */
+            Box(
+                modifier = Modifier
+                    .weight(0.5f) // цена
+                    .background(Color.Gray) // цвет
+                    .fillMaxHeight()
+            ) {
+                var inputValueX by remember { mutableStateOf("") }
+                var inputValueY by remember { mutableStateOf("") }
+                val isVisibleX by remember {
+                    derivedStateOf {
+                        inputValueX.isNotBlank()
+                    }
+                }
+                val isVisibleY by remember {
+                    derivedStateOf {
+                        inputValueY.isNotBlank()
+                    }
+                }
+                /* Окошко с вводом данных */
+                Column (
+                    horizontalAlignment = Alignment.Start,
                     verticalArrangement = Arrangement.Center
                 ) {
                     Text(
@@ -65,7 +140,8 @@ private class Controller {
                         fontSize = 20.sp,
                         modifier = Modifier.padding(5.dp)
                     )
-                    OutlinedTextField(
+                    /* Ввод X */
+                    OutlinedTextField (
                         value = inputValueX,
                         onValueChange = { newValue -> inputValueX = newValue },
                         modifier = Modifier
@@ -77,7 +153,7 @@ private class Controller {
                         singleLine = true,
                         trailingIcon = {
                             if (isVisibleX) {
-                                IconButton(
+                                IconButton (
                                     onClick = { inputValueX = "" }
                                 ) {
                                     Icon(
@@ -88,7 +164,8 @@ private class Controller {
                             }
                         }
                     )
-                    OutlinedTextField(
+                    /* Ввод Y */
+                    OutlinedTextField (
                         value = inputValueY,
                         onValueChange = { newValue -> inputValueY = newValue },
                         modifier = Modifier
@@ -100,7 +177,7 @@ private class Controller {
                         maxLines = 1,
                         trailingIcon = {
                             if (isVisibleY) {
-                                IconButton(
+                                IconButton (
                                     onClick = { inputValueY = "" }
                                 ) {
                                     Icon(
@@ -111,13 +188,15 @@ private class Controller {
                             }
                         }
                     )
-                    Button(
+                    /* кнопка отправить */
+                    Button (
                         onClick = {
-
+                            /* Тут как то нужно отправлять координаты в функцию рисования */
+                            /* Потом подумаем */
                         },
                         modifier = Modifier.padding(15.dp)
                     ) {
-                        Text(text = "Отправить")
+                        Text (text = "Отправить")
                     }
                 }
             }
@@ -125,19 +204,51 @@ private class Controller {
     }
 }
 
-@Preview
+/* На будущее */
+//class Controller {
+//    val fieldView = FieldView()
+//    @Composable
+//    fun userInput() {
+//        var inputValueX by remember { mutableStateOf("") }
+//        var inputValueY by remember { mutableStateOf("") }
+//        val isVisibleX by remember {
+//            derivedStateOf {
+//                inputValueX.isNotBlank()
+//            }
+//        }
+//        val isVisibleY by remember {
+//            derivedStateOf {
+//                inputValueY.isNotBlank()
+//            }
+//        }
+//        Surface (
+//            color = MaterialTheme.colors.background
+//        ) {
+//            Row {
+//                fieldView.drawField(10, 10)
+//                Box(
+//                    modifier = Modifier
+//                        .weight(1f)
+//                        .background(Color.Red)
+//                        .fillMaxHeight()
+//                ) {
+//                    // Пусто
+//                }
+//            }
+//        }
+//    }
+//}
+
+/* Main с созданием окна */
 fun main() = application {
-    val controller = Controller()
+    val controller = FieldView()
     Window (
-        title = "Initialisation",
+        title = "Algorithm A*",
         onCloseRequest = ::exitApplication,
         state = WindowState(
-            size = DpSize(1600.dp, 900.dp)
+            size = DpSize(1600.dp, 900.dp) // размеры экрана
         )
     ) {
-        controller.project()
+        controller.drawField(10, 10)
     }
-//    Window(onCloseRequest = ::exitApplication) {
-//        app()
-//    }
 }
