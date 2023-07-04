e38811dc84896b6ea9b80b241ce4c286493d9f50
1that <GeorgiOlio@yandex.ru>
2023-07-04 16:21:47 +0300

    Interface_Alpha1

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,25 +1,19 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.rounded.AccountBox
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.launch
+
 
 //fun app() {
 //    var text by remember { mutableStateOf("Hello, World!") }
@@ -49,64 +43,84 @@ private class Controller {
                 inputValueY.isNotBlank()
             }
         }
-        Column (
+        Box (
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
         ) {
-            Text (
-                text = "Введите размеры поля",
-                fontSize = 30.sp,
-                modifier = Modifier.padding(5.dp)
-            )
-            OutlinedTextField (
-                value = inputValueX,
-                onValueChange = { newValue -> inputValueX = newValue },
-                modifier = Modifier.padding(5.dp),
-                label = { Text(text = "Значение X") },
-                placeholder = { Text(text = "Введите данные") },
-                maxLines = 1,
-                trailingIcon = {
-                    if (isVisibleX) {
-                        IconButton(
-                            onClick = { inputValueX = "" }
-                        ) {
-                            Icon (
-                                imageVector = Icons.Default.Clear,
-                                contentDescription = "Clear"
-                            )
-                        }
-                    }
-                }
-            )
-            OutlinedTextField (
-                value = inputValueY,
-                onValueChange = { newValue -> inputValueY = newValue },
-                modifier = Modifier.padding(5.dp),
-                label = { Text(text = "Значение Y") },
-                placeholder = { Text(text = "Введите данные") },
-                maxLines = 1,
-                trailingIcon = {
-                    if (isVisibleY) {
-                        IconButton(
-                            onClick = { inputValueY = "" }
-                        ) {
-                            Icon (
-                                imageVector = Icons.Default.Clear,
-                                contentDescription = "Clear"
-                            )
-                        }
-                    }
-                }
-            )
-            Button(
-                onClick = {
-                },
-                modifier = Modifier.padding(15.dp)
+            Box (
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd
             ) {
-                Text(text = "Отправить")
+                Column(
+                    modifier = Modifier.padding(
+                        start = 20.dp,
+                        top = 20.dp,
+                        end = 20.dp,
+                        bottom = 20.dp
+                    ),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Введите размеры поля",
+                        fontSize = 20.sp,
+                        modifier = Modifier.padding(5.dp)
+                    )
+                    OutlinedTextField(
+                        value = inputValueX,
+                        onValueChange = { newValue -> inputValueX = newValue },
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .width(200.dp)
+                            .height(65.dp),
+                        label = { Text(text = "Значение X") },
+                        placeholder = { Text(text = "Введите данные") },
+                        singleLine = true,
+                        trailingIcon = {
+                            if (isVisibleX) {
+                                IconButton(
+                                    onClick = { inputValueX = "" }
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Clear,
+                                        contentDescription = "Clear"
+                                    )
+                                }
+                            }
+                        }
+                    )
+                    OutlinedTextField(
+                        value = inputValueY,
+                        onValueChange = { newValue -> inputValueY = newValue },
+                        modifier = Modifier
+                            .padding(5.dp)
+                            .width(200.dp)
+                            .height(65.dp),
+                        label = { Text(text = "Значение Y") },
+                        placeholder = { Text(text = "Введите данные") },
+                        maxLines = 1,
+                        trailingIcon = {
+                            if (isVisibleY) {
+                                IconButton(
+                                    onClick = { inputValueY = "" }
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Clear,
+                                        contentDescription = "Clear"
+                                    )
+                                }
+                            }
+                        }
+                    )
+                    Button(
+                        onClick = {
+
+                        },
+                        modifier = Modifier.padding(15.dp)
+                    ) {
+                        Text(text = "Отправить")
+                    }
+                }
             }
-            println("$inputValueX $inputValueY")
         }
     }
 }
@@ -116,7 +130,10 @@ fun main() = application {
     val controller = Controller()
     Window (
         title = "Initialisation",
-        onCloseRequest = ::exitApplication
+        onCloseRequest = ::exitApplication,
+        state = WindowState(
+            size = DpSize(1600.dp, 900.dp)
+        )
     ) {
         controller.project()
     }
